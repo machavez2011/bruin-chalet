db8b33220c0eb9cf8db7f7bb87ff0f0f50d6fb51
Michael Chavez <machavez2011@gmail.com>
2024-07-20 18:21:17 -0700

    bootstrap template incorprated into react application

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // need router
-import CalendarPicker from './components/calendar/calendar-picker';
+import LandingPage from './components/LandingPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CalendarPicker />
+    <LandingPage />
   </React.StrictMode>,
 )
