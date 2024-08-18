b444dc545878261280df4eabaebefbf9f2a82a7d
Michael Chavez <machavez2011@gmail.com>
2024-08-18 11:36:22 -0700

    reactDom test

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // need router
 import LandingPage from './components/LandingPage';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <LandingPage />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
