919c7ab06b2b1bd92f51b3ce4e4a4cf62fc4f514
Michael Chavez <michael.chavez@carvana.com>
2024-10-12 15:00:55 -0700

    footer width set

---
+++ src/modules/Footer/footer-styles.js
@@ -0,0 +1,6 @@
+import styled, { css } from 'styled-components';
+
+export const FooterWrapper = styled.div`
+    max-height: 703px;
+    overflow: hidden;
+`;
--- src/modules/Footer/footer.jsx
+++ src/modules/Footer/footer.jsx
@@ -1,12 +1,13 @@
 import Signup from "./components/Signup";
 import Contact from "./components/Contact";
+import { FooterWrapper } from "./footer-styles";
 
 const Footer = () => {
   return (
-    <>
+    <FooterWrapper>
       <Signup />
       <Contact />
-    </>
+    </FooterWrapper>
   );
 };
 
