bb72ec1519aed354b30aa577abc120a249873809
Michael Chavez <michael.chavez@carvana.com>
2024-10-12 15:38:27 -0700

    styling moved to js format

---
+++ src/modules/PhotosPage/photos-page-styles.js
@@ -0,0 +1,5 @@
+import styled, { css } from 'styled-components';
+
+export const PhotosWrapper = styled.div`
+    padding: 10rem 0;
+`;
--- src/modules/PhotosPage/photos-page.jsx
+++ src/modules/PhotosPage/photos-page.jsx
@@ -1,32 +1,28 @@
-import Footer from "../Footer";
-import * as SC from "./photos-page.css";
+import { PhotosWrapper } from "./photos-page-styles.js";
 
 const PhotosPage = () => {
   return (
-    <>
-      <div className="photos">
-        <p className="d-inline-flex gap-1">
-          <button
-            className="btn btn-primary"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseExample"
-            aria-expanded="false"
-            aria-controls="collapseExample"
-          >
-            Button with data-bs-target
-          </button>
-        </p>
-        <div className="collapse" id="collapseExample">
-          <div className="card card-body">
-            Some placeholder content for the collapse component. This panel is
-            hidden by default but revealed when the user activates the relevant
-            trigger.
-          </div>
+    <PhotosWrapper>
+      <p className="d-inline-flex gap-1">
+        <button
+          className="btn btn-primary"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseExample"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          Button with data-bs-target
+        </button>
+      </p>
+      <div className="collapse" id="collapseExample">
+        <div className="card card-body">
+          Some placeholder content for the collapse component. This panel is
+          hidden by default but revealed when the user activates the relevant
+          trigger.
         </div>
       </div>
-      <Footer />
-    </>
+    </PhotosWrapper>
   );
 };
 
